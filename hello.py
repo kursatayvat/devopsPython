494a5630793c4ae980fbc3704793538f6f10f497
Kürşat Ayvat <kursatayvat@kursats-mbp.mynetworksettings.com>
2023-04-01 13:25:32 -0400

    Initial Commit

---
+++ hello.py
@@ -0,0 +1,20 @@
+#start
+print("Hello World")
+
+# To comment multiple lines in python 
+# highligh the lines to be commented then press 
+# command + / (mac)
+
+print('Hello World')
+
+# There is no differents between enclosing the 
+#text with single Quotes or double quotes.
+
+#escape Character 
+#There are special characters used to insterst specific characters in 
+# a text. We  use escape characters are  preceded by backslash \
+#to include them in the text.
+
+
+print("He said \"It\'s a lovely day to learn pyton"")
+print("\\print\\")
